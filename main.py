b047ecfe3d7dc2d69237d963c06a024434adc24c
Jay Herron <needleinajaystack@protonmail.com>
2024-12-21 23:36:20 -0700

    feat: Updates to iOS sync file format

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+#! /usr/bin/python3
+
 import argparse
+from datetime import date, timedelta
 import re
 import os
 import shutil
 from pathlib import Path
 
-parser = argparse.ArgumentParser(description='Organize files with a YYYYMMDD name format into yearly and monthly folders in a destination directory.')
+parser = argparse.ArgumentParser(description='''
+Copy files from last month's directory (with format YYYY-MM) in the source into a YYYY/MM folder in the destination directory.
+This should be run on a monthly cadence.
+''')
 parser.add_argument('source', help='The folder that contains the files to organize')
 parser.add_argument('destination', help='The main folder that contains the destination folder organization')
 args = parser.parse_args()
 
-sourcePath = Path(args.source)
+sourcePath = p = Path(args.source)
 if not sourcePath.exists() or not sourcePath.is_dir():
   print('Source directory does not exist')
   exit(1)
@@ -18,30 +24,21 @@ destinationPath = p = Path(args.destination)
 if not destinationPath.exists() or not destinationPath.is_dir():
   print('Destination directory does not exist')
   exit(1)
-  
-for sourceFile in sourcePath.iterdir():
-  if not sourceFile.is_file:
-    continue
-  
-  regexMatch = re.search('(\d{4})(\d{2})(\d{2})\_.*', sourceFile.name)
-  if regexMatch is None:
-    print(f'File ignored: {sourceFile.name}')
-    continue
 
-  year = regexMatch.group(1)
-  month = regexMatch.group(2)
-  day = regexMatch.group(3)
+# Should reliably get the previous month. This should be run on a `@monthly` cron job.
+lastMonthDate = date.today() - timedelta(days=27)
+sourceDirName = f'{lastMonthDate.year}-{lastMonthDate.month}'
+sourceDir = sourcePath / sourceDirName
 
-  yearDir = destinationPath / year
-  if not yearDir.exists():
-    os.mkdir(yearDir)
-  
-  monthDir = yearDir / month
-  if not monthDir.exists():
-    os.mkdir(monthDir)
+yearDir = destinationPath / f'{lastMonthDate.year}'
+if not yearDir.exists():
+  os.mkdir(yearDir)
 
-  destinationFile = monthDir/sourceFile.name
-  shutil.move(sourceFile, destinationFile)
-  print(f'Moved {sourceFile} to {destinationFile}')
+monthDir = yearDir / f'{lastMonthDate.month}'
+if not monthDir.exists():
+  os.mkdir(monthDir)
+
+shutil.copytree(sourceDir, monthDir, dirs_exist_ok=True)
+print(f'Moved {sourceDir} to {monthDir}')
 
 print('Complete!')
